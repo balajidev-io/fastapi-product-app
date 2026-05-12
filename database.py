3a6a96813fd933b9e95c37d399e9b017f8d73e38
Balajipeddapalli <jib66558@gmail.com>
2026-05-12 18:02:48 +0530

    connected render database

--- database.py
+++ database.py
@@ -2,6 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql://postgres:admin123@localhost:5432/balajidev"
+db_url = "postgresql://fastapi_user:MgLDqyg1MjW0A8oA27qcJWuVnvJUe5Oy@dpg-d81hoggg4nts739h8f2g-a.oregon-postgres.render.com/fastapi_db_4zu0"
+
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
